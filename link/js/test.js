2129d3a62435de665145e2adf50a4cdaf73061b4
jide23work <90877938+jide23work@users.noreply.github.com>
2021-11-22 15:25:40 +0800

    update

---
+++ link/js/test.js
@@ -0,0 +1,582 @@
+var person = [
+    {
+        name: 'JUnit',
+        motto: '最著名的xUnit类的单元测试框架，但是不仅仅可以做单元测试。',
+        href: 'https://junit.org',
+        src: 'https://image.thum.io/get/https://junit.org',
+        sorts: 'kuangjia'
+    },
+    {
+        name: 'TestNG',
+        motto: '更强大的Java测试框架，灵活可扩展，支持注解和多线程。',
+        href: 'https://testng.org/',
+        src: 'https://image.thum.io/get/https://testng.org/',
+        sorts: 'kuangjia'
+    },
+    {
+        name: 'pytest',
+        motto: '最强大的Python测试框架，可定制性高，插件丰富。',
+        href: 'https://docs.pytest.org',
+        src: 'https://image.thum.io/get/https://docs.pytest.org',
+        sorts: 'kuangjia'
+    },
+    {
+        name: 'unittest',
+        motto: 'Python官方内置的测试框架。',
+        href: 'https://docs.python.org/3/library/unittest.html',
+        src: 'https://image.thum.io/get/https://docs.python.org/3/library/unittest.html',
+        sorts: 'kuangjia'
+    },
+    {
+        name: 'RobotFramework',
+        motto: '基于关键字驱动的测试框架，开源，易用。',
+        href: 'https://robotframework.org/',
+        src: 'https://image.thum.io/get/https://robotframework.org/',
+        sorts: 'kuangjia'
+    },
+    {
+        name: 'Cucumber',
+        motto: '开源的BDD自动化测试框架，支持Ruby和Java',
+        href: 'https://cucumber.io/',
+        src: 'https://image.thum.io/get/https://cucumber.io/',
+        sorts: 'kuangjia'
+    },
+    {
+        name: 'Behave',
+        motto: '基于Python的BDD自动化测试框架',
+        href: 'https://github.com/behave/behave',
+        src: 'https://image.thum.io/get/https://github.com/behave/behave',
+        sorts: 'kuangjia'
+    },
+    {
+        name: 'Gauge',
+        motto: '轻量级的跨平台测试自动化工具，可以以业务语言编写测试用例。',
+        href: 'https://gauge.org/index.html',
+        src: 'https://image.thum.io/get/https://gauge.org/index.html',
+        sorts: 'yongli'
+    },
+    {
+        name: 'graphwalker',
+        motto: '基于模型的测试用例生成框架',
+        href: 'https://github.com/GraphWalker',
+        src: 'https://image.thum.io/get/https://github.com/GraphWalker',
+        sorts: 'yongli'
+    },
+    {
+        name: 'PICT',
+        motto: '微软公司开发的pairwisetesting的用例生成工具',
+        href: 'https://docs.microsoft.com/en-us/previous-versions/software-testing/cc150619(v=msdn.10)',
+        src: 'https://image.thum.io/get/https://docs.microsoft.com/en-us/previous-versions/software-testing/cc150619(v=msdn.10)',
+        sorts: 'yongli'
+    },
+    {
+        name: 'TestLink',
+        motto: '开源测试用例管理，测试计划，测试执行，测试报告。',
+        href: 'http://www.testlink.org/',
+        src: 'https://image.thum.io/get/http://www.testlink.org/',
+        sorts: 'bug'
+    },
+    {
+        name: 'ZenTao',
+        motto: '禅道，国产开源工具，记录bug，用例管理，项目管理。',
+        href: 'https://www.zentao.net/',
+        src: 'https://image.thum.io/get/https://www.zentao.net/',
+        sorts: 'bug'
+    },
+    {
+        name: 'Redmine',
+        motto: '用Ruby开发的基于web的项目管理软件',
+        href: 'http://www.redmine.org/',
+        src: 'https://image.thum.io/get/http://www.redmine.org/',
+        sorts: 'bug'
+    },
+    {
+        name: 'jira',
+        motto: '缺陷跟踪、客户服务、需求收集、流程审批、任务跟踪、项目跟踪和敏捷管理。',
+        href: 'https://www.atlassian.com/software/jira',
+        src: 'https://image.thum.io/get/https://www.atlassian.com/software/jira',
+        sorts: 'bug'
+    },
+    {
+        name: 'Selenium',
+        motto: '业界最有影响力的Web自动化测试工具。',
+        href: 'https://www.seleniumhq.org/',
+        src: 'https://image.thum.io/get/https://www.seleniumhq.org/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'Cypress',
+        motto: '基于js的web自动化测试工具。',
+        href: 'https://www.cypress.io/',
+        src: 'https://image.thum.io/get/https://www.cypress.io/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'atalon',
+        motto: 'API，Web，移动端的自动化测试工具。',
+        href: 'https://www.katalon.com/',
+        src: 'https://image.thum.io/get/https://www.katalon.com/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'Ranorex',
+        motto: '商业GUI自动化测试工具，支持桌面，Web，移动端。',
+        href: 'https://www.ranorex.com/',
+        src: 'https://image.thum.io/get/https://www.ranorex.com/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'QTP/UFT',
+        motto: '商业GUI桌面，Web的自动化测试工具。',
+        href: 'https://en.wikipedia.org/wiki/HP_QuickTest_Professional)',
+        src: 'https://image.thum.io/get/https://en.wikipedia.org/wiki/HP_QuickTest_Professional)',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'AutoIT',
+        motto: '用类Basic脚本编写Windows桌面GUI自动化的工具。',
+        href: 'https://www.autoitscript.com/site/',
+        src: 'https://image.thum.io/get/https://www.autoitscript.com/site/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'Appium',
+        motto: '支持android和ios的移动端自动化测试工具.',
+        href: 'http://appium.io/',
+        src: 'https://image.thum.io/get/http://appium.io/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'STF',
+        motto: 'STF(smartphone/test/farm)移动设备管理工具，通过浏览器控制和管理移动设备。',
+        href: 'https://openstf.io/',
+        src: 'https://image.thum.io/get/https://openstf.io/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'Macaca',
+        motto: '面向多端的自动化测试工具，由阿里巴巴开源。',
+        href: 'https://macacajs.github.io/zh/',
+        src: 'https://image.thum.io/get/https://macacajs.github.io/zh/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'Airtest',
+        motto: 'UI自动化测试工具，支持App和游戏，网易开源。',
+        href: 'http://airtest.netease.com/',
+        src: 'https://image.thum.io/get/http://airtest.netease.com/',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'SoloPi',
+        motto: 'Soloπ是一个无线化、非侵入式的Android自动化工具，公测版拥有录制回放、性能测试、一机多控三项主要功能，能为测试开发人员节省宝贵时间。',
+        href: 'https://github.com/alipay/SoloPi',
+        src: 'https://image.thum.io/get/https://github.com/alipay/SoloPi',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'ChaosBlade',
+        motto: '阿里巴巴开源的一款简单易用、功能强大的混沌实验注入工具。',
+        href: 'https://github.com/chaosblade-io/chaosblade',
+        src: 'https://image.thum.io/get/https://github.com/chaosblade-io/chaosblade',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'ChaosMesh',
+        motto: '基于Kubernetes的云原生混沌工程平台。',
+        href: 'https://github.com/chaos-mesh/chaos-mesh',
+        src: 'https://image.thum.io/get/https://github.com/chaos-mesh/chaos-mesh',
+        sorts: 'zidonghua'
+    },
+    {
+        name: 'Apache Bench',
+        motto: 'HTTP性能测试工具',
+        href: 'http://httpd.apache.org/docs/2.4/programs/ab.html',
+        src: 'https://image.thum.io/get/http://httpd.apache.org/docs/2.4/programs/ab.html',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'LoadRunner',
+        motto: 'HP的商业性能测试工具',
+        href: 'https://ssl.www8.hp.com/sg/en/ad/load-runner/load-runner.html',
+        src: 'https://image.thum.io/get/https://ssl.www8.hp.com/sg/en/ad/load-runner/load-runner.html',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'JMeter',
+        motto: '基于Java的性能测试工具，开源。',
+        href: 'https://jmeter.apache.org/',
+        src: 'https://image.thum.io/get/https://jmeter.apache.org/',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'locust',
+        motto: '基于Python的性能测试工具',
+        href: 'https://www.locust.io/',
+        src: 'https://image.thum.io/get/https://www.locust.io/',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'Tsung',
+        motto: '基于Erlang的性能测试工具，支持海量的并发，但是由于懂Erlang的人少，故业界用得也少。',
+        href: 'http://tsung.erlang-projects.org/',
+        src: 'https://image.thum.io/get/http://tsung.erlang-projects.org/',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'Siege',
+        motto: 'Siege是一个多线程HTTP负载测试和基准测试工具',
+        href: 'https://www.joedog.org/',
+        src: 'https://image.thum.io/get/https://www.joedog.org/',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'wrk',
+        motto: '一款现代HTTP基准测试工具。',
+        href: 'https://github.com/wg/wrk',
+        src: 'https://image.thum.io/get/https://github.com/wg/wrk',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'htop',
+        motto: '实时统计Linux系统资源占用率',
+        href: 'https://hisham.hm/htop/',
+        src: 'https://image.thum.io/get/https://hisham.hm/htop/',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'iftop',
+        motto: '实时统计Linux网络流量',
+        href: 'https://en.wikipedia.org/wiki/Iftop',
+        src: 'https://image.thum.io/get/https://en.wikipedia.org/wiki/Iftop',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'nload',
+        motto: 'Linux网速实时监控',
+        href: 'https://linux.die.net/man/1/nload',
+        src: 'https://image.thum.io/get/https://linux.die.net/man/1/nload',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'webpagetest',
+        motto: '测量和分析网页性能工具,支持开发者下载源文件搭建私人的内部测试站点',
+        href: 'https://github.com/WPO-Foundation/webpagetest',
+        src: 'https://image.thum.io/get/https://github.com/WPO-Foundation/webpagetest',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'PageSpeed',
+        motto: '专注于改进网页性能的开发者工具,google出品。',
+        href: 'https://developers.google.cn/speed/pagespeed/insights/',
+        src: 'https://image.thum.io/get/https://developers.google.cn/speed/pagespeed/insights/',
+        sorts: 'xingneng'
+    },
+    {
+        name: 'jacoco',
+        motto: '开源的Java代码覆盖率工具',
+        href: 'https://www.eclemma.org/jacoco/',
+        src: 'https://image.thum.io/get/https://www.eclemma.org/jacoco/',
+        sorts: 'zhiliang'
+    },
+    {
+        name: 'SonarQube',
+        motto: '代码质量和安全的扫描工具',
+        href: 'https://www.sonarqube.org/',
+        src: 'https://image.thum.io/get/https://www.sonarqube.org/',
+        sorts: 'zhiliang'
+    },
+    {
+        name: 'CodePulse',
+        motto: '实时代码覆盖率工具',
+        href: 'http://code-pulse.com/',
+        src: 'https://image.thum.io/get/http://code-pulse.com/',
+        sorts: 'zhiliang'
+    },
+    {
+        name: 'Fiddler',
+        motto: 'Fiddler是一个HTTP调试抓包工具。它通过代理的方式获取程序http通讯的数据，可以用其检测网页和服务器的交互情况。',
+        href: 'https://www.telerik.com/fiddler',
+        src: 'https://image.thum.io/get/https://www.telerik.com/fiddler',
+        sorts: 'gongju'
+    },
+    {
+        name: 'Tcpdump',
+        motto: '网络抓包工具。',
+        href: 'https://linux.die.net/man/8/tcpdump',
+        src: 'https://image.thum.io/get/https://linux.die.net/man/8/tcpdump',
+        sorts: 'gongju'
+    },
+    {
+        name: 'Wireshark',
+        motto: '有界面的网络包分析工具，支持Windows/Linux/MacOS.',
+        href: 'https://www.wireshark.org/',
+        src: 'https://image.thum.io/get/https://www.wireshark.org/',
+        sorts: 'gongju'
+    },
+    {
+        name: 'GoReplay',
+        motto: '网络监控工具，可以做流量回放，压力测试，流量分析',
+        href: 'https://goreplay.org/',
+        src: 'https://image.thum.io/get/https://goreplay.org/',
+        sorts: 'gongju'
+    },
+    {
+        name: 'TcpCopy',
+        motto: '流量回放工具。',
+        href: 'https://github.com/session-replay-tools/tcpcopy',
+        src: 'https://image.thum.io/get/https://github.com/session-replay-tools/tcpcopy',
+        sorts: 'gongju'
+    },
+    {
+        name: 'Charles',
+        motto: '抓包，弱网模拟，接口测试。',
+        href: 'https://www.charlesproxy.com/',
+        src: 'https://image.thum.io/get/https://www.charlesproxy.com/',
+        sorts: 'gongju'
+    },
+    {
+        name: 'TC',
+        motto: 'Linux上控制网络流量的工具',
+        href: 'https://linux.die.net/man/8/tc)',
+        src: 'https://image.thum.io/get/https://linux.die.net/man/8/tc)',
+        sorts: 'gongju'
+    },
+    {
+        name: 'WANem',
+        motto: '广域网网络模拟器\网络损伤模拟仪硬件网络损伤仿真仪器，用于模拟广域网的各种弱网条件，例如丢包，延迟，低带宽等。',
+        href: 'http://wanem.sourceforge.net/',
+        src: 'https://image.thum.io/get/http://wanem.sourceforge.net/',
+        sorts: 'gongju'
+    },
+    {
+        name: 'Postman',
+        motto: 'Postman',
+        href: 'https://www.getpostman.com/',
+        src: 'https://image.thum.io/get/https://www.getpostman.com/',
+        sorts: 'jiekou'
+    },
+    {
+        name: 'SoapUI',
+        motto: '最流行的API测试工具。',
+        href: 'https://www.soapui.org/',
+        src: 'https://image.thum.io/get/https://www.soapui.org/',
+        sorts: 'jiekou'
+    },
+    {
+        name: 'JMeter',
+        motto: '工业级的压力测试工具，也有人拿它做接口测试',
+        href: 'https://jmeter.apache.org/',
+        src: 'https://image.thum.io/get/https://jmeter.apache.org/',
+        sorts: 'jiekou'
+    },
+    {
+        name: 'Fiddler',
+        motto: '是一个HTTP调试抓包工具。它通过代理的方式获取程序http通讯的数据，可以用其检测网页和服务器的交互情况。',
+        href: 'https://www.telerik.com/fiddler',
+        src: 'https://image.thum.io/get/https://www.telerik.com/fiddler',
+        sorts: 'jiekou'
+    },
+    {
+        name: 'Hitchhiker',
+        motto: '是一款开源的支持多人协作的api测试工具，支持自动化测试/数据对比，压力测试，支持脚本定制请求，可以轻松部署到本地，和你的team成员一起协作测试Api。',
+        href: 'https://github.com/brookshi/Hitchhiker',
+        src: 'https://image.thum.io/get/https://github.com/brookshi/Hitchhiker',
+        sorts: 'jiekou'
+    },
+    {
+        name: 'Rest-Assured',
+        motto: '测试Java类库',
+        href: 'https://github.com/rest-assured/rest-assured',
+        src: 'https://image.thum.io/get/https://github.com/rest-assured/rest-assured',
+        sorts: 'jiekou'
+    },
+    {
+        name: 'Pact',
+        motto: '消费者驱动契约测试的一种实现，可以用来在消费者的代码里mock响应，在提供方的代码中验证交互。',
+        href: 'https://github.com/pact-foundation/pact-specification',
+        src: 'https://image.thum.io/get/https://github.com/pact-foundation/pact-specification',
+        sorts: 'jiekou'
+    },
+    {
+        name: 'ExtentReports',
+        motto: '一个优雅，互动，灵活细致的测试报告框架。',
+        href: 'http://extentreports.com/',
+        src: 'https://image.thum.io/get/http://extentreports.com/',
+        sorts: 'baogao'
+    },
+    {
+        name: 'Allure',
+        motto: '测试报告框架',
+        href: 'http://allure.qatools.ru/',
+        src: 'https://image.thum.io/get/http://allure.qatools.ru/',
+        sorts: 'baogao'
+    },
+    {
+        name: 'cucumber-reporting',
+        motto: 'Cucumber',
+        href: 'https://github.com/damianszczepanik/cucumber-reporting',
+        src: 'https://image.thum.io/get/https://github.com/damianszczepanik/cucumber-reporting',
+        sorts: 'baogao'
+    },
+    {
+        name: 'ReportNG',
+        motto: 'TestNG',
+        href: 'https://reportng.uncommons.org/',
+        src: 'https://image.thum.io/get/https://reportng.uncommons.org/',
+        sorts: 'baogao'
+    },
+    {
+        name: 'Moco',
+        motto: '基于Java的Mock开源框架',
+        href: 'https://github.com/dreamhead/moco',
+        src: 'https://image.thum.io/get/https://github.com/dreamhead/moco',
+        sorts: 'mock'
+    },
+    {
+        name: 'mockserver',
+        motto: '基于Java的Mock开源框架，可以通过client或者restfulAPI远程控制mockserver的返回。',
+        href: 'https://github.com/jamesdbloom/mockserver',
+        src: 'https://image.thum.io/get/https://github.com/jamesdbloom/mockserver',
+        sorts: 'mock'
+    },
+    {
+        name: 'Mockito',
+        motto: '用于Java单元测试的Mock测试框架',
+        href: 'https://site.mockito.org/',
+        src: 'https://image.thum.io/get/https://site.mockito.org/',
+        sorts: 'mock'
+    },
+    {
+        name: 'PowerMock',
+        motto: '基于Mockito扩展的Java单元测试Mock框架',
+        href: 'https://github.com/powermock/powermock/',
+        src: 'https://image.thum.io/get/https://github.com/powermock/powermock/',
+        sorts: 'mock'
+    },
+    {
+        name: 'Whistle',
+        motto: '是基于',
+        href: 'https://github.com/avwo/whistle/',
+        src: 'https://image.thum.io/get/https://github.com/avwo/whistle/',
+        sorts: 'mock'
+    },
+    {
+        name: 'Node',
+        motto: 'server回放。',
+        href: '实现的跨平台抓包调试代理工具，可以录制请求和响应，然后作为mock',
+        src: 'https://image.thum.io/get/实现的跨平台抓包调试代理工具，可以录制请求和响应，然后作为mock',
+        sorts: 'mock'
+    },
+    {
+        name: 'vm-sandbox-repeater',
+        motto: '阿里开源的基于JVM-Sandbox的流量录制回放工具，提供入口请求（HTTP/Dubbo/Java）流量回放、子调用（Java/Dubbo）返回值Mock能力。',
+        href: 'https://github.com/alibaba/jvm-sandbox-repeater',
+        src: 'https://image.thum.io/get/https://github.com/alibaba/jvm-sandbox-repeater',
+        sorts: 'huifang'
+    },
+]
+var inp = document.getElementsByClassName('inp')[0],
+    btnList = document.getElementsByClassName('btn-list')[0],
+    prompt = document.getElementsByClassName('prompt')[0],
+    user = document.getElementsByClassName('user')[0];
+
+function createList(list) {
+    var str = '';
+    list.forEach(function(ele, index) {
+        str += '<a href =' + ele.href + ' target="_self">\
+                    <li>\
+                    <div class="head-img">\
+                    <img src=' + ele.src + ' alt="">\
+                    </div>\
+                    <div class="tit">\
+                    <p class="name">' + ele.name + '</p>\
+                    <p class="motto">' + ele.motto + '</p>\
+                    </div>\
+                    </li>\
+                </a>'
+    })
+    user.innerHTML = str
+}
+createList(person);
+var state = createStore({
+    text: '',
+    sorts: 'all'
+})
+state.addFn(function() {
+    createList(perArr());
+    anti(show());
+})
+inp.oninput = anti(getInput, 1000)
+
+function getInput() {
+    state.change({
+        type: 'text',
+        value: this.value
+    });
+}
+
+function anti(hander, delay) {
+    var timer = null;
+    return function() {
+        var _self = this,
+            args = arguments;
+        clearTimeout(timer)
+        timer = setTimeout(function() {
+            hander.apply(_self, args)
+        }, delay)
+    }
+}
+
+function show() {
+    if (user.children.length == 0) {
+        prompt.style.display = 'block'
+    } else {
+        prompt.style.display = 'none'
+    }
+}
+
+function addFn(obj, arr) {
+    return function() {
+        var lastArr = arr;
+        for (prop in obj) {
+            lastArr = obj[prop](state.getState()[prop], lastArr)
+        }
+        return lastArr
+    }
+}
+var perArr = addFn({
+    text: filterText,
+    sorts: filtersorts
+}, person)
+btnList.onclick = function(e) {
+    var tar = e.target;
+    if (tar.tagName == 'OPTION') {
+        document.getElementsByClassName('active')[0].className = '';
+        tar.className = 'active';
+        state.change({
+            type: 'sorts',
+            value: tar.getAttribute('sorts')
+        })
+    }
+}
+
+function filterText(text, arr) {
+    return arr.filter(function(ele, index) {
+        if (ele.name.indexOf(text) != -1 || ele.motto.indexOf(text) != -1) {
+            return true
+        }
+    })
+}
+
+function filtersorts(sorts, arr) {
+    if (sorts == 'all') {
+        return arr
+    } else {
+        return arr.filter(function(ele, index) {
+            if (ele.sorts == sorts) {
+                return true
+            }
+        })
+    }
+}
