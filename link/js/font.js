021d504576b8cbda11ea87d4ab385f9f7f754278
jide23work <90877938+jide23work@users.noreply.github.com>
2021-11-28 10:17:20 +0800

    Update font.js

--- link/js/font.js
+++ link/js/font.js
@@ -1,150 +1,165 @@
 var person = [{
-	name:  '书法字体',
-	motto:  '在线书法字体转换_毛笔书法字体生成_矢量书法字典在线查询',
-	href:  'http://www.shufaziti.com/',
-	src: 'https://image.thum.io/get/http://www.shufaziti.com/',
-	sorts:  'fonts'
-},
-{
-	name:  '字体网站|Fonts',
-	motto:  '我们的Web字体库提供了一系列样式，可以涵盖您网站的任何项目。看看我们的新websafe字体保持在趋势的顶端',
-	href:  'https://www.fonts.com/web-fonts',
-	src: 'https://image.thum.io/get/https://www.fonts.com/web-fonts',
-	sorts:  'fonts'
-},
-{
-	name:  '说文解字',
-	motto:  '說文解字，在線查詢，篆書字典，說文解字注，在線查詢',
-	href:  'http://www.shuowen.org/',
-	src: 'https://image.thum.io/get/http://www.shuowen.org/',
-	sorts:  'fonts'
-},
-{
-	name:  '求字体',
-	motto:  '字体下载-求字体网提供中文和英文字体库下载、识别与预览服务，找字体的好帮手',
-	href:  'http://www.qiuziti.com/',
-	src: 'https://image.thum.io/get/http://www.qiuziti.com/',
-	sorts:  'fonts'
-},
-{
-	name:  'Googlefonts',
-	motto:  'Google字体是一个非常好的开源项目，它不但提供了丰富的字体，更提供了方便快捷的使用方式',
-	href:  'http://www.googlefonts.cn',
-	src: 'https://image.thum.io/get/http://www.googlefonts.cn',
-	sorts:  'fonts'
-},
-{
-	name:  'FontsinUse',
-	motto:  '在线字体效果展示',
-	href:  'https://fontsinuse.com/',
-	src: 'https://image.thum.io/get/https://fontsinuse.com/',
-	sorts:  'fonts'
-},
-{
-	name:  'Fontsup',
-	motto:  '免费英文字体下载库',
-	href:  'https://fontsup.com/',
-	src: 'https://image.thum.io/get/https://fontsup.com/',
-	sorts:  'fonts'
-},
-{
-	name:  'Fonts2u',
-	motto:  '详细分类免费字体搜索下载库',
-	href:  'http://zh.fonts2u.com/',
-	src: 'https://image.thum.io/get/http://zh.fonts2u.com/',
-	sorts:  'fonts'
-},
-{
-	name:  'Urbanfonts',
-	motto:  '免费英文字体搜索下载站',
-	href:  'https://www.urbanfonts.com/',
-	src: 'https://image.thum.io/get/https://www.urbanfonts.com/',
-	sorts:  'fonts'
-},
-{
-	name:  'myfonts',
-	motto:  '最新时尚的商业英文字体',
-	href:  'http://www.myfonts.com/',
-	src: 'https://image.thum.io/get/http://www.myfonts.com/',
-	sorts:  'fonts'
-},
-{
-	name:  '苹方字体',
-	motto:  '苹果最新的系统界面字体SanFrancisco',
-	href:  'https://developer.apple.com/fonts/',
-	src: 'https://image.thum.io/get/https://developer.apple.com/fonts/',
-	sorts:  'fonts'
-},
-{
-	name:  'Comicneue',
-	motto:  '免费手写可爱字体官网下载',
-	href:  'http://comicneue.com/',
-	src: 'https://image.thum.io/get/http://comicneue.com/',
-	sorts:  'fonts'
-},
-{
-	name:  'Typekit',
-	motto:  'Adobe出品的web在线字库',
-	href:  'https://typekit.com/',
-	src: 'https://image.thum.io/get/https://typekit.com/',
-	sorts:  'fonts'
-},
-{
-	name:  '思源宋体',
-	motto:  'Adobe、Google出品的开源中文宋体',
-	href:  'https://source.typekit.com/source-han-serif/',
-	src: 'https://image.thum.io/get/https://source.typekit.com/source-han-serif/',
-	sorts:  'fonts'
-},
-{
-	name:  '360查字体',
-	motto:  '查版权，免纠纷',
-	href:  'https://fonts.safe.360.cn/',
-	src: 'https://image.thum.io/get/https://fonts.safe.360.cn/',
-	sorts:  'fonts'
-},
-{
-	name:  'UrbanFonts',
-	motto:  '字体资源搜索与下载。',
-	href:  'http://www.urbanfonts.com',
-	src: 'https://image.thum.io/get/http://www.urbanfonts.com',
-	sorts:  'fonts'
-},
-{
-	name:  'DaFont',
-	motto:  '字体资源搜索与下载。',
-	href:  'http://www.dafont.com',
-	src: 'https://image.thum.io/get/http://www.dafont.com',
-	sorts:  'fonts'
-},
-{
-	name:  'FontM',
-	motto:  '字体资源搜索与下载。',
-	href:  'http://fontm.com',
-	src: 'https://image.thum.io/get/http://fontm.com',
-	sorts:  'fonts'
-},
-{
-	name:  'fontsquirrel',
-	motto:  '字体松鼠，可商用的免费字体',
-	href:  'http://www.fontsquirrel.com',
-	src: 'https://image.thum.io/get/http://www.fontsquirrel.com',
-	sorts:  'wangye'
-},
-{
-	name:  'FontFabric',
-	motto:  '提供漂亮的免费字体下载',
-	href:  'http://fontfabric.com',
-	src: 'https://image.thum.io/get/http://fontfabric.com',
-	sorts:  'wangye'
-},
-{
-	name:  'LostType',
-	motto:  '非常多漂亮的英文字体，带banner展示',
-	href:  'http://www.losttype.com/browse',
-	src: 'https://image.thum.io/get/http://www.losttype.com/browse',
-	sorts:  'wangye'
-},]
+        name:   '书法字体',
+        motto:   '在线书法字体转换_毛笔书法字体生成_矢量书法字典在线查询',
+        href:   'http://www.shufaziti.com/',
+        src: 'https://image.thum.io/get/http://www.shufaziti.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   '字体网站|Fonts',
+        motto:   '我们的Web字体库提供了一系列样式，可以涵盖您网站的任何项目。看看我们的新websafe字体保持在趋势的顶端',
+        href:   'https://www.fonts.com/web-fonts',
+        src: 'https://image.thum.io/get/https://www.fonts.com/web-fonts',
+        sorts:   'fonts'
+    },
+    {
+        name:   '说文解字',
+        motto:   '說文解字，在線查詢，篆書字典，說文解字注，在線查詢',
+        href:   'http://www.shuowen.org/',
+        src: 'https://image.thum.io/get/http://www.shuowen.org/',
+        sorts:   'fonts'
+    },
+    {
+        name:   '求字体',
+        motto:   '字体下载-求字体网提供中文和英文字体库下载、识别与预览服务，找字体的好帮手',
+        href:   'http://www.qiuziti.com/',
+        src: 'https://image.thum.io/get/http://www.qiuziti.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'Googlefonts',
+        motto:   'Google字体是一个非常好的开源项目，它不但提供了丰富的字体，更提供了方便快捷的使用方式',
+        href:   'http://www.googlefonts.cn',
+        src: 'https://image.thum.io/get/http://www.googlefonts.cn',
+        sorts:   'fonts'
+    },
+    {
+        name:   'FontsinUse',
+        motto:   '在线字体效果展示',
+        href:   'https://fontsinuse.com/',
+        src: 'https://image.thum.io/get/https://fontsinuse.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'Fontsup',
+        motto:   '免费英文字体下载库',
+        href:   'https://fontsup.com/',
+        src: 'https://image.thum.io/get/https://fontsup.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'Fonts2u',
+        motto:   '详细分类免费字体搜索下载库',
+        href:   'http://zh.fonts2u.com/',
+        src: 'https://image.thum.io/get/http://zh.fonts2u.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'Urbanfonts',
+        motto:   '免费英文字体搜索下载站',
+        href:   'https://www.urbanfonts.com/',
+        src: 'https://image.thum.io/get/https://www.urbanfonts.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'myfonts',
+        motto:   '最新时尚的商业英文字体',
+        href:   'http://www.myfonts.com/',
+        src: 'https://image.thum.io/get/http://www.myfonts.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   '苹方字体',
+        motto:   '苹果最新的系统界面字体SanFrancisco',
+        href:   'https://developer.apple.com/fonts/',
+        src: 'https://image.thum.io/get/https://developer.apple.com/fonts/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'Comicneue',
+        motto:   '免费手写可爱字体官网下载',
+        href:   'http://comicneue.com/',
+        src: 'https://image.thum.io/get/http://comicneue.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'Typekit',
+        motto:   'Adobe出品的web在线字库',
+        href:   'https://typekit.com/',
+        src: 'https://image.thum.io/get/https://typekit.com/',
+        sorts:   'fonts'
+    },
+    {
+        name:   '思源宋体',
+        motto:   'Adobe、Google出品的开源中文宋体',
+        href:   'https://source.typekit.com/source-han-serif/',
+        src: 'https://image.thum.io/get/https://source.typekit.com/source-han-serif/',
+        sorts:   'fonts'
+    },
+    {
+        name:   '360查字体',
+        motto:   '查版权，免纠纷',
+        href:   'https://fonts.safe.360.cn/',
+        src: 'https://image.thum.io/get/https://fonts.safe.360.cn/',
+        sorts:   'fonts'
+    },
+    {
+        name:   'UrbanFonts',
+        motto:   '字体资源搜索与下载。',
+        href:   'http://www.urbanfonts.com',
+        src: 'https://image.thum.io/get/http://www.urbanfonts.com',
+        sorts:   'fonts'
+    },
+    {
+        name:   'DaFont',
+        motto:   '字体资源搜索与下载。',
+        href:   'http://www.dafont.com',
+        src: 'https://image.thum.io/get/http://www.dafont.com',
+        sorts:   'fonts'
+    },
+    {
+        name:   'FontM',
+        motto:   '字体资源搜索与下载。',
+        href:   'http://fontm.com',
+        src: 'https://image.thum.io/get/http://fontm.com',
+        sorts:   'fonts'
+    },
+    {
+        name:   'fontsquirrel',
+        motto:   '字体松鼠，可商用的免费字体',
+        href:   'http://www.fontsquirrel.com',
+        src: 'https://image.thum.io/get/http://www.fontsquirrel.com',
+        sorts:   'wangye'
+    },
+    {
+        name:   'FontFabric',
+        motto:   '提供漂亮的免费字体下载',
+        href:   'http://fontfabric.com',
+        src: 'https://image.thum.io/get/http://fontfabric.com',
+        sorts:   'wangye'
+    },
+    {
+        name:   'LostType',
+        motto:   '非常多漂亮的英文字体，带banner展示',
+        href:   'http://www.losttype.com/browse',
+        src: 'https://image.thum.io/get/http://www.losttype.com/browse',
+        sorts:   'wangye'
+    },
+    {
+        name:   '阿里巴巴字体',
+        motto:   '阿里巴巴字体素材平台',
+        href:   'https://alibabafont.taobao.com/',
+        src: 'https://image.thum.io/get/https://alibabafont.taobao.com/',
+        sorts:   'wangye'
+    },
+    {
+        name:   '查字体商用',
+        motto:   'github上查商用，类似360查字体',
+        href:   'https://zenozeng.github.io/Free-Chinese-Fonts/',
+        src: 'https://image.thum.io/get/https://zenozeng.github.io/Free-Chinese-Fonts/',
+        sorts:   'wangye'
+    }
+]
 var inp = document.getElementsByClassName('inp')[0],
     btnList = document.getElementsByClassName('btn-list')[0],
     prompt = document.getElementsByClassName('prompt')[0],
